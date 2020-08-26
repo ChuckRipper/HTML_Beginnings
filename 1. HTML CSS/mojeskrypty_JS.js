66622178532e8b5c7244c4f506315d76addd2380
ChuckRipper <53383803+ChuckRipper@users.noreply.github.com>
2020-08-26 05:13:06 +0200

    1 HTML Project (HTML, CSS)
    Ver. 1.0

---
+++ 1. HTML CSS/mojeskrypty_JS.js
@@ -0,0 +1,10 @@
+document.getElementById('title_1').innerHTML = "Moj tytul z JS";
+/*  modyfikacja h1 - skrypt JS
+        Metody osadzania JS:
+        1 - można wstawić w <head> ale trzeba pamiętać że strona ładuje się od góry do dołu kodu, 
+        więc skrypt może się odwoływać do elementów, które jeszcze nie istnieją, 
+        dlatego najlepiej jest wstawić JS skrypt dopiero za <body>
+        2 - za body
+        3 - stosując opóźnienia w ładowaniu skryptów (dopiszę o tym później)
+        4 - w osobnym pliku, dołączając w <head> linijkę "<script src="mojeskrypty_JS.js"></script>"
+*/
